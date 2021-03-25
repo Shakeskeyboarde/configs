350fc22e344248a09e22f24ab7d51aa5feb1cc36
Chris Ackerman <chris@topher.land>
2021-03-24 17:32:19 -0700

    Added StrictMode wrapper to SPA template.

--- templates/index-spa.tsx
+++ templates/index-spa.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 
-render(<div>Hello, world!</div>, document.getElementById('root'));
+render(
+  <StrictMode>
+    <div>Hello, world!</div>
+  </StrictMode>,
+  document.getElementById('root'),
+);
